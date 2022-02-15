c253f65be2a7abe91a061a35c660511625a5d151
Sainath Bommalakunta <P7164753@ness.com>
2022-02-15 18:44:14 +0530

    added test console app for testing log security

---
+++ serilog-test/Program.cs
@@ -0,0 +1,37 @@
+﻿using Serilog;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.SystemConsole.Themes;
+using System;
+using System.Threading;
+
+namespace serilog_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                 .MinimumLevel.Verbose()
+                 .WriteTo.Console(new JsonFormatter())
+                 .CreateLogger();
+
+            try
+            {
+                Log.Debug("Getting started");
+
+                Log.Information("Hello {Name} from thread {ThreadId}", Environment.GetEnvironmentVariable("USERNAME"), Thread.CurrentThread.ManagedThreadId);
+
+                Log.Warning("No coins remain at position {@Position}", new { Lat = 25, Long = 134 });
+
+                Log.Security("this is securty issue", new { app="a1", module="m1" });
+
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Something went wrong");
+            }
+
+            Log.CloseAndFlush();
+        }
+    }
+}
